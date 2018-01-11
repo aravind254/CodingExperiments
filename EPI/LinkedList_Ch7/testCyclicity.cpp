8505b8d9492ce463c3d70bb4055f26ed9609ed91
Aravind <aravind@Aravinds-MacBook-Pro.local>
2018-01-11 08:41:27 -0800

    LL V6

---
+++ EPI/LinkedList_Ch7/testCyclicity.cpp
@@ -0,0 +1,84 @@
+#include "LL.h"
+using namespace std;
+
+typedef std::shared_ptr<ListNode<int>> ln;
+
+ln testCyclicity(const LL<int> &list)
+{
+ ln fPtr = list.head,sPtr = list.head;
+ while(fPtr)
+ {
+   fPtr=fPtr->next;
+   if(!fPtr){
+    return nullptr;
+   } 
+   fPtr=fPtr->next;
+   if(!fPtr){
+    return nullptr;
+   }
+  sPtr = sPtr->next;
+  if(fPtr == sPtr){
+  cout << "Found a cycle" << endl;
+
+  // Cycle length
+  fPtr = fPtr->next;
+  int cyclen = 1;
+  while(fPtr != sPtr)
+  {
+   fPtr = fPtr->next;
+   cyclen++;
+  }
+  cout << "Cycle length = " << cyclen << endl;
+
+  // Start of Cycle
+  ln iter1 = list.head;
+  ln iter2 = list.head;
+  while(cyclen)
+  {
+    iter1 = iter1->next;
+    --cyclen;
+  }
+
+  while(iter1 != iter2)
+  {
+     iter1 = iter1->next;
+     iter2 = iter2->next;
+  }
+  
+  cout << "Found Start of Cycle at node = " << iter1->data << endl;
+  return iter1;
+  } 
+ }
+
+ return nullptr; 
+}
+
+
+
+int main()
+{
+ LL<int> myList;
+ myList.add(1);
+ myList.add(2);
+ myList.add(3);
+ myList.add(4);
+ myList.add(5);
+
+
+ // Form a cycle
+ ln curNode = myList.head;
+ while(curNode->next){
+ curNode = curNode->next;
+ }
+ curNode->next = (myList.head)->next;
+ 
+  
+
+
+ //myList.print();
+ ln node = testCyclicity(myList);
+ if(!node){
+  cout << "Cycle not found" << endl;
+ }
+  
+}
